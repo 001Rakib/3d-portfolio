7df2181d1f11a4654814b351a1aa56c0c51398b3
001Rakib <rakibrakibrakib354@gmail.com>
2025-04-13 23:04:28 +0600

    implemented project showcase section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/sections/Hero";
+import ShowcaseSection from "@/sections/ShowcaseSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <ShowcaseSection />
     </>
   );
 }
